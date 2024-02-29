f5deb6fd7e7642dadd706b11f1169bb727fee6c1
raccoonjeong <rana3782@gmail.com>
2024-02-29 09:35:48 +0900

    추리게임 하는중

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,16 +1,19 @@
-use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
     
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
+    // println!("The secret number is: {secret_number}");
 
     println!("Please input your guess.");
     
     let mut guess = String::new();
+
+    let guess = guess.trim().parse().expect("Please type a number!");
 
     io::stdin()
        .read_line(&mut guess)
@@ -18,5 +21,12 @@ fn main() {
 
     println!("You guessed: {guess}");
 
-    // https://doc.rust-kr.org/ch02-00-guessing-game-tutorial.html 임의의 숫자 생성하기까지
+    match guess.cmp($secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
+
+    // https://doc.rust-kr.org/ch02-00-guessing-game-tutorial.html 추가된 라인은 다음과 같습니다:
+
 }
